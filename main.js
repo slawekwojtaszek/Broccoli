9d40d13f60065a34a54cc009b7cba3ecf039b727
Slawek Wojtaszek <wojtaszek@protonmail.com>
2022-03-16 21:22:08 +0000

    Add photos, fix mobile menu, add nutrients section

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 const button = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-const list = document.querySelector(".list");
+const list = document.querySelector(".menu");
+
 button.addEventListener("click", () => {
    console.log("elo");
-
+   button.classList.toggle("open");
    menu.classList.toggle("menu");
    list.classList.toggle("slide-in-top");
 });
-
-let parent = document.querySelector(".sticky").parentElement;
-
-while (parent) {
-   const hasOverflow = getComputedStyle(parent).overflow;
-   if (hasOverflow !== "visible") {
-      console.log(hasOverflow, parent);
-   }
-   parent = parent.parentElement;
-}
