54722cc4bb55a833242e683473bd07c2e0de722c
Slawek Wojtaszek <wojtaszek@protonmail.com>
2022-03-18 23:37:17 +0000

    Add more projects, save night mode in local storage

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const button = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-const list = document.querySelector(".menu");
 
 button.addEventListener("click", () => {
    console.log("elo");
    button.classList.toggle("open");
    menu.classList.toggle("menu");
-   list.classList.toggle("slide-in-top");
+   menu.classList.toggle("slide-in-top");
 });
